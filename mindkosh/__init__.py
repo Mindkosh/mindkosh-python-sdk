3d1c814823d09e36fae00cad3c035481c9540403
Shikhar Dev Gupta <sdevgupta@mindkosh.com>
2026-08-14 15:53:15 +0530

    Updated to v1.1.4

--- mindkosh/__init__.py
+++ mindkosh/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2023 Mindkosh Technologies. All rights reserved.
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 from mindkosh.client import Client
 from mindkosh.annotations.testset import TestSet
 from mindkosh.label import Label
